99e165fb7687d2ef0bd75af7b6e910532eb416c5
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-07 15:33:01 +0800

    feat: modify middlewares and change it to object

---
+++ src/middlewares/index.js
@@ -0,0 +1,47 @@
+const ServiceGuestReviews = require('../services/ServiceGuestReviews');
+const ServiceSentimentAnalytics = require('../services/ServiceSentimentAnalytics');
+
+const middleware = {
+	logger: (req, _res, next) => {
+		console.log(`${req.method} ${req.url}`);
+		next();
+	},
+
+	attachGetAllReviewsToRequest: async (req, _res, next) => {
+		try {
+			const service = new ServiceGuestReviews();
+
+			const reviews = await service.getAllReviews(req.query);
+			req.data = reviews;
+			next();
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	attachGetSentimentCategoriesToRequest: async (req, _res, next) => {
+		try {
+			const service = new ServiceSentimentAnalytics();
+
+			const categories = await service.getSentimentAnalyticsCategories();
+			req.data = categories;
+			next();
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	attachGetSentimentCategoryItemsToRequest: async (req, _res, next) => {
+		try {
+			const service = new ServiceSentimentAnalytics();
+
+			const categoryItems = await service.getSentimentAnalyticsCategoryItems(req.params.category, req.query);
+			req.data = categoryItems;
+			next();
+		} catch (error) {
+			next(error);
+		}
+	},
+};
+
+module.exports = middleware;
--- src/middlewares/logger.middleware.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, _res, next) => {
-	console.log(`${req.method} ${req.url}`);
-	next();
-};
-
-module.exports = { logger };
