ed3794a16926f5d998f591241f6a80fe9a475055
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-07 15:31:39 +0800

    feat: modify configuration for database

---
+++ src/configs/configDatabase.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, APP_NAME } = process.env;
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority&appname=${APP_NAME}`;
+
+async function connectDB() {
+	try {
+		await mongoose.connect(url);
+		console.log('Connected to the database, ', url);
+	} catch (error) {
+		console.error('Error connecting to the database: ', error);
+	}
+}
+
+module.exports = { connectDB };
