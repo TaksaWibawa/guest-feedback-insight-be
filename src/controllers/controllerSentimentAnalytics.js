43540f3f8516057b782168ba6a6ba598dc877131
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-03-21 12:52:50 +0800

    chore: add controller for sentiment statistics

--- src/controllers/controllerSentimentAnalytics.js
+++ src/controllers/controllerSentimentAnalytics.js
@@ -31,6 +31,21 @@ const controllerSentimentAnalytics = {
 			res.status(500).json({ message: error.message });
 		}
 	},
+
+	async getSentimentStatistics(req, res) {
+		try {
+			if (!req.data || req.data.length === 0) {
+				return res.status(404).json({ data: [], message: 'Data not found', status: 404 });
+			}
+			res.status(200).json({
+				data: req.data,
+				status: 200,
+				message: 'Data found',
+			});
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		}
+	},
 };
 
 module.exports = controllerSentimentAnalytics;
